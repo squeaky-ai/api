bc0bf2aff90132c04a0ad0a45f2ccf55a984dfb8
Lewis Monteith <lewismonteith@gmail.com>
2021-05-04 19:53:09 +0100

    Prefix routes under /api

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@
 
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount GraphqlPlayground::Rails::Engine, at: '/playground', graphql_path: '/graphql'
+    mount GraphqlPlayground::Rails::Engine, at: '/playground', graphql_path: '/api/graphql'
   end
 
-  get '/ping', to: 'ping#index'
-
-  post '/graphql', to: 'graphql#execute'
+  scope '/api' do
+    get '/ping', to: 'ping#index'
+    post '/graphql', to: 'graphql#execute'
+  end
 end
