2a1645ab01fddf7e0608eb3c42216437593ba501
Lewis Monteith <lewismonteith@gmail.com>
2021-05-05 21:08:19 +0100

    Add test around team model

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,5 +3,65 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#owner?' do
+    context 'when the user is the owner' do
+      let(:instance) { described_class.new(role: 2) }
+
+      it 'returns true' do
+        expect(instance.owner?).to be true
+      end
+    end
+
+    context 'when the user is not the owner' do
+      let(:instance) { described_class.new(role: 1) }
+
+      it 'returns false' do
+        expect(instance.owner?).to be false
+      end
+    end
+  end
+
+  describe '#admin?' do
+    context 'when the user is the owner' do
+      let(:instance) { described_class.new(role: 2) }
+
+      it 'returns true' do
+        expect(instance.admin?).to be true
+      end
+    end
+
+    context 'when the user is an admin' do
+      let(:instance) { described_class.new(role: 1) }
+
+      it 'returns true' do
+        expect(instance.admin?).to be true
+      end
+    end
+
+    context 'when the user is a member' do
+      let(:instance) { described_class.new(role: 0) }
+
+      it 'returns false' do
+        expect(instance.admin?).to be false
+      end
+    end
+  end
+
+  describe '#pending?' do
+    context 'when the user has accepted the invite' do
+      let(:instance) { described_class.new(status: 0) }
+
+      it 'returns false' do
+        expect(instance.pending?).to be false
+      end
+    end
+
+    context 'when the user has not accepted the invite' do
+      let(:instance) { described_class.new(status: 1) }
+
+      it 'returns true' do
+        expect(instance.pending?).to be true
+      end
+    end
+  end
 end
