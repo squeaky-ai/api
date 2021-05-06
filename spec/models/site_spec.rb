37f5aa5cc4b99bcd7138f51f4a6e7c5d1dd186bd
Lewis Monteith <lewismonteith@gmail.com>
2021-05-06 08:43:05 +0100

    Add tests around site model

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -3,5 +3,69 @@
 require 'rails_helper'
 
 RSpec.describe Site, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.format_uri' do
+    context 'when no uri is provided' do
+      it 'raises an exception' do
+        expect { Site.format_uri(nil) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when the uri is not invalid' do
+      it 'returns nil' do
+        uri = 'dfgdfgdf'
+        expect(Site.format_uri(uri)).to be nil
+      end
+    end
+
+    context 'when the uri is valid' do
+      it 'returns the uri' do
+        uri = 'https://squeaky.ai'
+        expect(Site.format_uri(uri)).to eq uri
+      end
+
+      it 'strips the path if it exists' do
+        uri = 'https://squeaky.ai/pricing'
+        expect(Site.format_uri(uri)).to eq 'https://squeaky.ai'
+      end
+
+      it 'strips the query string if it exists' do
+        uri = 'https://squeaky.ai?teapot=kettle'
+        expect(Site.format_uri(uri)).to eq 'https://squeaky.ai'
+      end
+    end
+  end
+
+  describe '#owner_name' do
+    let(:instance) { described_class.create(name: Faker::Company.name, url: Faker::Internet.url, plan: 0) }
+
+    let(:first_name) { Faker::Name.first_name }
+    let(:last_name) { Faker::Name.last_name }
+
+    before do
+      user = User.create(first_name: first_name, last_name: last_name, email: Faker::Internet.email)
+      Team.create(role: 2, user: user, site: instance)
+    end
+
+    it 'returns the owners full name' do
+      expect(instance.owner_name).to eq "#{first_name} #{last_name}"
+    end
+  end
+
+  describe '#admins' do
+    let(:instance) { described_class.create(name: Faker::Company.name, url: Faker::Internet.url, plan: 0) }
+
+    before do
+      Team.create(role: 0, user: User.new, site: instance)
+      Team.create(role: 1, user: User.new, site: instance)
+      Team.create(role: 2, user: User.new, site: instance)
+      instance.reload
+    end
+
+    it 'returns only the team members that are admins' do
+      admins = instance.admins
+
+      expect(admins.size).to eq 2
+      admins.each { |a| expect(a.admin?).to be true }
+    end
+  end
 end
