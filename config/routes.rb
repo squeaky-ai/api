317c7537f90bb3bb3f045676046b3b63c09b5af1
Lewis Monteith <lewismonteith@gmail.com>
2021-05-18 19:28:23 +0100

    Enable playground in prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount GraphqlPlayground::Rails::Engine, at: '/', graphql_path: '/api/graphql' if Rails.env.development?
+  mount GraphqlPlayground::Rails::Engine, at: '/', graphql_path: '/api/graphql' # if Rails.env.development?
 
   scope '/api' do
     get '/ping', to: 'ping#index'
