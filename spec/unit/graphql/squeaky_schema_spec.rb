d2fcc3890ae0a414e227bae82adb344e9410e7b9
Lewis Monteith <lewismonteith@gmail.com>
2021-05-25 15:53:18 +0100

    Add spec around the schema for coverage

---
+++ spec/unit/graphql/squeaky_schema_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe SqueakySchema do
+  describe '.resolve_type' do
+    it 'raises an error because we should handle it at the type level' do
+      expect { described_class.resolve_type(nil, nil, nil) }.to raise_error GraphQL::RequiredImplementationMissingError
+    end
+  end
+end
