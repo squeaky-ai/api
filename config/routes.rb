20bbe7ffc6fb227b56dd67563823cfafec99e80f
Lewis Monteith <lewismonteith@gmail.com>
2021-06-12 20:21:14 +0100

    Fix the devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Enable the GraphQL playground
-  mount GraphqlPlayground::Rails::Engine,
-        at: '/api/playground',
-        graphql_path: '/api/graphql'
+  scope 'api' do
+    get 'ping', to: 'ping#index'
+    post 'graphql', to: 'graphql#execute'
 
-  scope '/api' do
-    get '/ping', to: 'ping#index'
-    post '/graphql', to: 'graphql#execute'
+    # Enable the GraphQL playground
+    mount GraphqlPlayground::Rails::Engine, at: 'playground', graphql_path: 'graphql'
 
-    # Override the devise controllers so that they
-    # only respond to json
-    # devise_for :users, controllers: {
-    #   sessions: 'auth/sessions',
-    #   registrations: 'auth/registrations'
-    # }
-
-    scope '/auth' do
+    # Custom devise routes that are more suited to the front end
+    scope 'auth' do
       devise_scope :user do
         post 'sign_in', to: 'auth/sessions#create', as: :user_session
         post 'sign_up', to: 'auth/registrations#create', as: :user_registration
