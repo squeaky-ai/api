6ac93b890d5028c6eac173ab868761d02f9bd032
Lewis Monteith <lewismonteith@gmail.com>
2021-06-14 15:52:41 +0100

    Add full name to graphql

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -7,6 +7,7 @@ module Types
     field :id, ID, null: false
     field :first_name, String, null: true
     field :last_name, String, null: true
+    field :full_name, String, null: true
     field :email, String, null: false
     field :created_at, String, null: false
     field :updated_at, String, null: true
