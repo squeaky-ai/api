b1d58e990fba08c7d929fd8547c8689150278a77
Lewis Monteith <lewismonteith@gmail.com>
2021-06-24 13:16:52 +0100

    Add coverage for user deletion mailer

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail.from).to eq ['hello@squeaky.ai']
     end
   end
+
+  describe 'destroyed' do
+    let(:user) { create_user }
+    let(:mail) { described_class.destroyed(user.email) }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq 'Account Deletion Confirmed'
+      expect(mail.to).to eq [user.email]
+      expect(mail.from).to eq ['hello@squeaky.ai']
+    end
+  end
 end
