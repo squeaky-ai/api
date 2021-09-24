054e621bbc55c13528002a784a4268b3bfa82541
Lewis Monteith <lewismonteith@gmail.com>
2021-09-24 19:59:24 +0100

    Revert

--- app/graphql/types/recordings_extension.rb
+++ app/graphql/types/recordings_extension.rb
@@ -2,74 +2,54 @@
 
 module Types
   # The 'recordings' field on the site is handled here as
-  # we only want to load the data if it is requested. The
-  # source of truth is in the database, but this is grabbed
-  # from elasticsearch
+  # we only want to load the data if it is requested
   class RecordingsExtension < GraphQL::Schema::FieldExtension
     def apply
       field.argument(:page, Integer, required: false, default_value: 0, description: 'The page of results to get')
       field.argument(:size, Integer, required: false, default_value: 15, description: 'The page size')
       field.argument(:query, String, required: false, default_value: '', description: 'The search query')
-      field.argument(:sort, RecordingSortType, required: false, default_value: 'connected_at__asc', description: 'The sort order')
+      field.argument(:sort, RecordingSortType, required: false, default_value: 'DATE_DESC', description: 'The sort order')
     end
 
     def resolve(object:, arguments:, **_rest)
-      search = search(arguments, object.object.id)
-      results = SearchClient.search(index: Recording::INDEX, body: search)
+      order = order_by(arguments[:sort])
+
+      recordings = Site
+                   .find(object.object.id)
+                   .recordings
+                   .eager_load(:visitor, :pages)
+                   .where('deleted IS false AND (session_id ILIKE :query OR locale ILIKE :query OR useragent ILIKE :query)', { query: "%#{arguments[:query]}%" })
+                   .order(order)
+                   .page(arguments[:page])
+                   .per(arguments[:size])
 
       {
-        items: items(results),
-        pagination: pagination(arguments, results, arguments[:size])
+        items: recordings,
+        pagination: pagination(arguments, recordings, arguments[:size])
       }
     end
 
     private
 
-    def search(arguments, site_id)
-      params = {
-        from: (arguments[:page] - 1) * arguments[:size],
-        size: arguments[:size],
-        sort: sort(arguments),
-        query: {
-          bool: {
-            must: [
-              { term: { site_id: { value: site_id } } }
-            ]
-          }
-        }
-      }
-
-      unless arguments[:query].empty?
-        params[:query][:bool][:filter] = [
-          { query_string: { query: "*#{arguments[:query]}*" } }
-        ]
-      end
-
-      params
-    end
-
-    def sort(arguments)
-      parts = arguments[:sort].split('__')
-      sort = {}
-
-      sort[parts.first] = {
-        unmapped_type: 'date_nanos',
-        order: parts.last
-      }
-
-      sort
-    end
-
-    def items(results)
-      results['hits']['hits'].map { |r| r['_source'] }
-    end
-
-    def pagination(arguments, results, size)
+    def pagination(arguments, recordings, size)
       {
         page_size: size,
-        total: results['hits']['total']['value'],
+        total: recordings.total_count,
         sort: arguments[:sort]
       }
+    end
+
+    def order_by(sort)
+      orders = {
+        'DATE_DESC' => 'connected_at DESC',
+        'DATE_ASC' => 'connected_at ASC',
+        'DURATION_DESC' => 'disconnected_at - connected_at DESC',
+        'DURATION_ASC' => 'disconnected_at - connected_at ASC',
+        'PAGE_SIZE_DESC' => 'array_length(page_views, 1) DESC', # TODO
+        'PAGE_SIZE_ASC' => 'array_length(page_views, 1) ASC' # TODO
+      }
+
+      Arel.sql(orders[sort] || orders['DATE_DESC'])
     end
   end
 end
