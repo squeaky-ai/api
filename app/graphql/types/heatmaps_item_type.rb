48e20cdf41738eba930697343470f97191c1e63a
Lewis Monteith <lewismonteith@gmail.com>
2021-11-04 14:50:29 +0000

    Make x and y nullable

--- app/graphql/types/heatmaps_item_type.rb
+++ app/graphql/types/heatmaps_item_type.rb
@@ -4,8 +4,8 @@ module Types
   class HeatmapsItemType < Types::BaseObject
     description 'The heatmaps item'
 
-    field :x, Integer, null: false
-    field :y, Integer, null: false
+    field :x, Integer, null: true
+    field :y, Integer, null: true
     field :selector, String, null: true
   end
 end
