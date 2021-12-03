bd731a5e892b07b250960877796e82c054e2a4a3
Lewis Monteith <lewismonteith@gmail.com>
2021-12-03 12:02:45 +0000

    Set defaults for everything for feedback

--- app/graphql/mutations/feedback/update.rb
+++ app/graphql/mutations/feedback/update.rb
@@ -27,7 +27,20 @@ module Mutations
       end
 
       def resolve(**args)
-        feedback = @site.feedback || ::Feedback.create(site_id: @site.id)
+        feedback = @site.feedback || ::Feedback.create(
+          site_id: @site.id,
+          nps_enabled: false,
+          nps_accent_color: '#0074E0',
+          nps_schedule: 'once',
+          nps_phrase: @site.name,
+          nps_follow_up_enabled: true,
+          nps_contact_consent_enabled: false,
+          nps_layout: 'full_width',
+          sentiment_enabled: false,
+          sentiment_accent_color: '#0074E0',
+          sentiment_excluded_pages: [],
+          sentiment_layout: 'right_middle'
+        )
 
         feedback.assign_attributes(args.except(:site_id))
         feedback.save
