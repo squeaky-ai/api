2b5bce64b96e393174b7a32e973aae7520ea9a2b
Lewis Monteith <lewismonteith@gmail.com>
2021-12-07 11:24:40 +0000

    Include missing browser details for nps

--- app/graphql/types/feedback/nps_response_item.rb
+++ app/graphql/types/feedback/nps_response_item.rb
@@ -11,6 +11,7 @@ module Types
       field :contact, Boolean, null: false
       field :email, String, null: true
       field :visitor, Types::Visitors::Visitor, null: false
+      field :device, Types::Recordings::Device, null: false
       field :session_id, String, null: false
       field :recording_id, String, null: false
       field :timestamp, String, null: false
