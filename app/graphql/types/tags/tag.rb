d73804f48b120309ce210438f542a614db66cced
Lewis Monteith <lewismonteith@gmail.com>
2021-12-28 21:19:31 +0000

    Remove unecessary date

--- app/graphql/types/tags/tag.rb
+++ app/graphql/types/tags/tag.rb
@@ -7,8 +7,6 @@ module Types
 
       field :id, ID, null: false
       field :name, String, null: false
-      field :created_at, String, null: false
-      field :updated_at, String, null: true
     end
   end
 end
