b4fa7f59eec8a46fbeb1896ed157582b6e08b8ca
Lewis Monteith <lewismonteith@gmail.com>
2022-01-20 10:15:55 +0000

    Improve perf of analytics recordings

--- app/graphql/resolvers/analytics/referrers.rb
+++ app/graphql/resolvers/analytics/referrers.rb
@@ -3,38 +3,32 @@
 module Resolvers
   module Analytics
     class Referrers < Resolvers::Base
-      type [Types::Analytics::Referrer, { null: true }], null: false
+      type Types::Analytics::Referrers, null: false
 
-      def resolve
-        referrers.each_with_object([]) do |val, memo|
-          name = val['referrer'] || 'Direct'
-          existing = memo.find { |m| m[:name] == name }
+      argument :page, Integer, required: false, default_value: 0
+      argument :size, Integer, required: false, default_value: 10
 
-          if existing
-            existing[:count] += 1
-          else
-            memo.push({ name:, count: 1 })
-          end
-        end
-      end
+      def resolve(page:, size:)
+        referrers = Recording
+                    .where(
+                      'site_id = ? AND to_timestamp(disconnected_at / 1000)::date BETWEEN ? AND ? AND status IN (?)',
+                      object[:site_id],
+                      object[:from_date],
+                      object[:to_date],
+                      [Recording::ACTIVE, Recording::DELETED]
+                    )
+                    .select('DISTINCT(COALESCE(referrer, \'Direct\')) referrer, count(*) count')
+                    .page(page)
+                    .per(size)
+                    .group('referrer')
 
-      private
-
-      def referrers
-        sql = <<-SQL
-          SELECT referrer
-          FROM recordings
-          WHERE recordings.site_id = ? AND to_timestamp(recordings.disconnected_at / 1000)::date BETWEEN ? AND ? AND recordings.status IN (?)
-        SQL
-
-        variables = [
-          object[:site_id],
-          object[:from_date],
-          object[:to_date],
-          [Recording::ACTIVE, Recording::DELETED]
-        ]
-
-        Sql.execute(sql, variables)
+        {
+          items: referrers,
+          pagination: {
+            page_size: size,
+            total: referrers.total_count
+          }
+        }
       end
     end
   end
