ccf93ca2c93bc020f530218720c077057f902080
Lewis Monteith <lewismonteith@gmail.com>
2022-01-20 12:38:37 +0000

    Fetch the page percentage on the server

--- app/graphql/types/analytics/page.rb
+++ app/graphql/types/analytics/page.rb
@@ -8,6 +8,7 @@ module Types
       field :path, String, null: false
       field :count, Integer, null: false
       field :avg, Integer, null: false
+      field :percentage, Integer, null: false
     end
   end
 end
