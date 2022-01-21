6aaef5999ba57b97c21738bd57f57aa0c54ae9da
Lewis Monteith <lewismonteith@gmail.com>
2022-01-21 10:31:29 +0000

    Try and group dimensions better

--- app/graphql/resolvers/analytics/dimensions.rb
+++ app/graphql/resolvers/analytics/dimensions.rb
@@ -3,14 +3,14 @@
 module Resolvers
   module Analytics
     class Dimensions < Resolvers::Base
-      type [Types::Analytics::Dimension, { null: true }], null: false
+      type [Types::Analytics::Dimension, { null: true }], null: false 
 
       def resolve
         sql = <<-SQL
-          SELECT DISTINCT(ROUND(device_x, -1)) device_x, count(*) count
+          SELECT DISTINCT(ROUND(device_x, -1)) grouped_device_x, count(*) count
           FROM recordings
           WHERE recordings.device_x > 0 AND recordings.site_id = ? AND to_timestamp(recordings.disconnected_at / 1000)::date BETWEEN ? AND ? AND recordings.status IN (?)
-          GROUP BY device_x
+          GROUP BY grouped_device_x
         SQL
 
         variables = [
@@ -20,7 +20,14 @@ module Resolvers
           [Recording::ACTIVE, Recording::DELETED]
         ]
 
-        Sql.execute(sql, variables)
+        results = Sql.execute(sql, variables)
+
+        results.map do |result|
+          {
+            device_x: result['grouped_device_x'],
+            count: result['count']
+          }
+        end
       end
     end
   end
