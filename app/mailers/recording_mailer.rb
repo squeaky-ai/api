221e80504458a544fad37db43d4716fbb3e72595
Lewis Monteith <lewismonteith@gmail.com>
2022-05-12 18:03:15 +0100

    Add recording emails and specs

---
+++ app/mailers/recording_mailer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class RecordingMailer < ApplicationMailer
+  def first_recording(site_id)
+    @site = Site.find_by(id: site_id)
+
+    return unless @site
+
+    emails = owner_and_admin_emails
+
+    mail(to: emails, subject: 'Your first Squeaky recording is in! 👀') unless emails.empty?
+  end
+
+  def first_recording_followup(site_id)
+    @site = Site.find_by(id: site_id)
+
+    return unless @site
+
+    emails = owner_and_admin_emails
+
+    mail(to: emails, subject: 'Capture only the right data') unless emails.empty?
+  end
+
+  private
+
+  def owner_and_admin_emails
+    members =@site.team.filter do |team|
+      [Team::OWNER, Team::ADMIN].include?(team.role) && team.user.communication_enabled?(:onboarding_email)
+    end
+
+    members.map { |m| m.user.email }
+  end
+end
---
+++ spec/mailers/recording_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe RecordingMailer, type: :mailer do
+  describe '#first_recording' do
+    describe 'when the site no longer exists' do
+      let(:mail) { described_class.first_recording(23423423) }
+
+      it 'does not send anything' do
+        expect(mail.to).to eq nil
+      end
+    end
+    
+    describe 'when the site exists' do
+      let(:site) { create(:site_with_team) }
+      let(:mail) { described_class.first_recording(site.id) }
+
+      let(:team_1) { create(:team, site:, role: Team::ADMIN) }
+      let(:team_2) { create(:team, site:, role: Team::ADMIN) }
+      let(:team_3) { create(:team, site:, role: Team::MEMBER) }
+
+      before do
+        create(:communication, user: team_1.user, onboarding_email: true)
+        create(:communication, user: team_2.user, onboarding_email: false)
+        create(:communication, user: team_3.user, onboarding_email: true)
+      end
+
+      it 'sends the email to owners and admins that have preferences enabled' do
+        expect(mail.subject).to eq 'Your first Squeaky recording is in! 👀'
+        expect(mail.from).to eq ['hello@squeaky.ai']
+        expect(mail.to).to eq [site.owner.user.email, team_1.user.email]
+      end
+    end
+  end
+end
