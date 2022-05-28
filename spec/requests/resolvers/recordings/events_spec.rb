6605c8ed6352850ba8dec5091d9dab4dfd29e98c
Lewis Monteith <lewismonteith@gmail.com>
2022-05-28 18:04:15 +0100

    Read events from the database again

--- spec/requests/resolvers/recordings/events_spec.rb
+++ spec/requests/resolvers/recordings/events_spec.rb
@@ -22,28 +22,12 @@ site_recording_events_query = <<-GRAPHQL
 GRAPHQL
 
 RSpec.describe Resolvers::Recordings::Events, type: :request do
-  context 'when the events are stored in S3' do
+  context 'when there no events' do
     let(:user) { create(:user) }
     let(:site) { create(:site_with_team, owner: user) }
-    let(:recording) { create(:recording, site: site) }
 
-    let(:list_objects_v2) do
-      double(contents: [
-        {
-          key: "#{site.uuid}/#{recording.visitor.visitor_id}/#{recording.session_id}.json"
-        }
-      ])
-    end
-
-    let(:get_object) do
-      events_fixtures = require_fixture('events.json')
-      double(body: double(read: events_fixtures.map(&:to_json).to_json))
-    end
-
-    let(:s3_client) { instance_double(Aws::S3::Client, list_objects_v2:, get_object:) }
-
-    before do
-      allow(Aws::S3::Client).to receive(:new).and_return(s3_client)
+    let(:recording) do
+      create(:recording, site: site)
     end
 
     subject do
@@ -53,102 +37,51 @@ RSpec.describe Resolvers::Recordings::Events, type: :request do
 
     it 'returns the item with the events' do
       response = subject['data']['site']['recording']
-      expect(response['events']['items'].size).to eq 88
+      expect(response['events']['items']).to eq []
     end
 
     it 'returns the correct pagination' do
       response = subject['data']['site']['recording']
       expect(response['events']['pagination']).to eq(
-        'perPage' => -1,
-        'itemCount' => -1,
+        'perPage' => 250,
+        'itemCount' => 0,
         'currentPage' => 1,
-        'totalPages' => 1
+        'totalPages' => 0
       )
     end
   end
 
-  context 'when the events are stored in the database' do
-    context 'and there no events' do
-      let(:user) { create(:user) }
-      let(:site) { create(:site_with_team, owner: user) }
-      let(:recording) { create(:recording, site: site) }
+  context 'when there are some events' do
+    let(:user) { create(:user) }
+    let(:site) { create(:site_with_team, owner: user) }
 
-      let(:list_objects_v2) { double(contents: []) }
-      let(:s3_client) { instance_double(Aws::S3::Client, list_objects_v2:) }
+    let(:recording) do
+      rec = create(:recording, site: site)
 
-      before do
-        allow(Aws::S3::Client).to receive(:new).and_return(s3_client)
-      end 
+      data = { href: "http://localhost/", width: 0, height: 0 }
+      rec.events << Event.new(event_type: Event::META, data: data, timestamp: 1625389200000)
 
-      subject do
-        variables = { site_id: site.id, recording_id: recording.id }
-        graphql_request(site_recording_events_query, variables, user)
-      end
-
-      it 'returns the item with the events' do
-        response = subject['data']['site']['recording']
-        expect(response['events']['items']).to eq []
-      end
-
-      it 'returns the correct pagination' do
-        response = subject['data']['site']['recording']
-        expect(response['events']['pagination']).to eq(
-          'perPage' => 500,
-          'itemCount' => 0,
-          'currentPage' => 1,
-          'totalPages' => 0
-        )
-      end
+      rec
     end
 
-    context 'and there are some events' do
-      let(:user) { create(:user) }
-      let(:site) { create(:site_with_team, owner: user) }
-      let(:recording) { create(:recording, site: site) }
+    subject do
+      variables = { site_id: site.id, recording_id: recording.id }
+      graphql_request(site_recording_events_query, variables, user)
+    end
 
-      let(:list_objects_v2) { double(contents: []) }
-      let(:s3_client) { instance_double(Aws::S3::Client, list_objects_v2:) }
+    it 'returns the item with the events' do
+      response = subject['data']['site']['recording']
+      expect(response['events']['items']).to eq ["{\"id\":#{recording.events.first.id},\"data\":{\"href\":\"http://localhost/\",\"width\":0,\"height\":0},\"type\":4,\"timestamp\":1625389200000}"]
+    end
 
-      before do
-        now = Time.now
-        events_fixture = require_fixture('events.json')
-
-        events = events_fixture.map do |fixture|
-          event = JSON.parse(fixture)['value']
-          {
-            data: event['data'],
-            event_type: event['type'],
-            timestamp: event['timestamp'],
-            recording_id: recording.id,
-            created_at: now,
-            updated_at: now
-          }
-        end
-
-        Event.insert_all(events)
-
-        allow(Aws::S3::Client).to receive(:new).and_return(s3_client)
-      end
-
-      subject do
-        variables = { site_id: site.id, recording_id: recording.id }
-        graphql_request(site_recording_events_query, variables, user)
-      end
-
-      it 'returns the item with the events' do
-        response = subject['data']['site']['recording']
-        expect(response['events']['items'].size).to eq 88
-      end
-
-      it 'returns the correct pagination' do
-        response = subject['data']['site']['recording']
-        expect(response['events']['pagination']).to eq(
-          'perPage' => 500,
-          'itemCount' => 88,
-          'currentPage' => 1,
-          'totalPages' => 1
-        )
-      end
+    it 'returns the correct pagination' do
+      response = subject['data']['site']['recording']
+      expect(response['events']['pagination']).to eq(
+        'perPage' => 250,
+        'itemCount' => 1,
+        'currentPage' => 1,
+        'totalPages' => 1
+      )
     end
   end
 end
