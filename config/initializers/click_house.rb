c6623fd021a4919a4d73a3c8716b7f3f17444610
Lewis Monteith <lewismonteith@gmail.com>
2022-06-13 11:33:19 +0100

    Add clickhouse monkey patch for string results

--- config/initializers/click_house.rb
+++ config/initializers/click_house.rb
@@ -4,3 +4,26 @@ ClickHouse.config do |config|
   config.logger = Rails.logger
   config.assign(Rails.application.config_for('click_house'))
 end
+
+module ClickHouse
+  module Response
+    class Factory
+      def self.[](faraday)
+        body = faraday.body
+        # Monkey patch this JSON parse as sometimes it
+        # is returning as a string
+        # https://github.com/shlima/click_house/issues/17
+        body = JSON.parse(body) if body.is_a?(String)
+
+        return body if !body.is_a?(Hash) || !(body.key?('meta') && body.key?('data'))
+
+        ResultSet.new(
+          meta: body.fetch('meta'),
+          data: body.fetch('data'),
+          totals: body['totals'],
+          statistics: body['statistcs']
+        )
+      end
+    end
+  end
+end
