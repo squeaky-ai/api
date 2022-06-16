b57f38b4c20ac5b6d005f3c3519b20f534227d68
Lewis Monteith <lewismonteith@gmail.com>
2022-06-16 17:28:56 +0100

    Add schema for events counts

---
+++ app/graphql/resolvers/events/counts.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Resolvers
+  module Events
+    class Counts < Resolvers::Base
+      type Types::Events::Counts, null: false
+
+      argument :group_ids, [ID], required: true, default_value: []
+      argument :capture_ids, [ID], required: true, default_value: []
+      argument :from_date, GraphQL::Types::ISO8601Date, required: true
+      argument :to_date, GraphQL::Types::ISO8601Date, required: true
+
+      def resolve(group_ids:, capture_ids:, from_date:, to_date:)
+        date_format, group_type, group_range = Charts.date_groups(from_date, to_date)
+
+        puts '@@', group_ids, capture_ids, date_format
+
+        {
+          group_type:,
+          group_range:,
+          items: []
+        }
+      end
+    end
+  end
+end
