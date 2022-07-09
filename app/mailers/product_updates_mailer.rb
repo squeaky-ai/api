364a65d9e9b82e7daf6b0d4abf2e8626d929d062
Lewis Monteith <lewismonteith@gmail.com>
2022-07-09 09:19:52 +0100

    Email updates

--- app/mailers/product_updates_mailer.rb
+++ app/mailers/product_updates_mailer.rb
@@ -3,10 +3,11 @@
 class ProductUpdatesMailer < ApplicationMailer
   def q2_2022(user) # rubocop:disable Naming/VariableNumber
     @user = user
+    @unsubscribable = true
 
     return unless user.communication_enabled?(:product_updates_email)
     return unless user.first_name.present?
 
-    mail(to: user.email, subject: 'Product Update: Q2 2022.')
+    mail(to: user.email, subject: 'Product Update: Q2 2022')
   end
 end
