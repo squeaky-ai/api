42ab59859864669085bc42fed0aa40b36c504388
Lewis Monteith <lewismonteith@gmail.com>
2022-07-15 15:19:57 +0100

    Add clickhouse ping to healthcheck

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -4,9 +4,10 @@ class PingController < ApplicationController
   def index
     ActiveRecord::Base.connection.verify!
     Cache.redis.ping
+    ClickHouse.connection.ping
     render plain: 'PONG'
   rescue StandardError => e
-    logger.error(e)
+    logger.fatal(e)
     render plain: 'NOT PONG!', status: 500
   end
 end
