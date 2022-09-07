d2183b45ec8c9f7aaf00d09d9d979e68449b6ae3
Lewis Monteith <lewismonteith@gmail.com>
2022-09-07 20:35:19 +0100

    Fix failing specs

--- spec/jobs/events_processing_job_spec.rb
+++ spec/jobs/events_processing_job_spec.rb
@@ -24,51 +24,56 @@ RSpec.describe EventsProcessingJob, type: :job do
     let!(:event_4) { create(:event_capture, site:, event_type: EventCapture::ERROR, rules: [rule_4]) }
     let!(:event_5) { create(:event_capture, site:, event_type: EventCapture::CUSTOM, rules: [rule_5]) }
 
-    let(:data) {
-      [
-        {
-          type: Event::PAGE_VIEW,
-          data: { href: '/' },
-          source: nil
-        },
-        {
-          type: Event::INCREMENTAL_SNAPSHOT,
-          data: { text: 'Add to cart' },
-          source: 2
-        },
-        {
-          type: Event::INCREMENTAL_SNAPSHOT,
-          data: { selector: 'html > body' },
-          source: 2
-        },
-        {
-          type: Event::ERROR,
-          data: { message: 'Error: Oh no!' },
-          source: nil
-        },
-        {
-          type: Event::CUSTOM_TRACK,
-          data: { name: 'my-event' },
-          source: nil
-        }
-      ]
-    }
-
     before do
       allow(Time).to receive(:now).and_return(now)
 
-      ClickHouse::Event.insert do |buffer|
-        data.each.with_index do |d, index|
-          buffer << {
-            uuid: SecureRandom.uuid,
-            site_id: site.id,
-            recording_id: recording.id,
-            type: d[:type],
-            source: d[:source],
-            data: d[:data].to_json,
-            timestamp: Time.new(2022, 7, 6, 5, 0, 0).to_i * 1000 + index
-          }
-        end
+      timestamp = Time.new(2022, 7, 6, 5, 0, 0).to_i * 1000
+
+      ClickHouse::PageEvent.insert do |buffer|
+        buffer << {
+          uuid: SecureRandom.uuid,
+          site_id: site.id,
+          recording_id: recording.id,
+          url: '/',
+          exited_at: timestamp
+        }
+      end
+
+      ClickHouse::ClickEvent.insert do |buffer|
+        buffer << {
+          uuid: SecureRandom.uuid,
+          site_id: site.id,
+          recording_id: recording.id,
+          text: 'Add to cart',
+          timestamp:
+        }
+        buffer << {
+          uuid: SecureRandom.uuid,
+          site_id: site.id,
+          recording_id: recording.id,
+          selector: 'html > body',
+          timestamp:
+        }
+      end
+
+      ClickHouse::ErrorEvent.insert do |buffer|
+        buffer << {
+          uuid: SecureRandom.uuid,
+          site_id: site.id,
+          recording_id: recording.id,
+          message: 'Error: Oh no!',
+          timestamp:
+        }
+      end
+
+      ClickHouse::CustomEvent.insert do |buffer|
+        buffer << {
+          uuid: SecureRandom.uuid,
+          site_id: site.id,
+          recording_id: recording.id,
+          name: 'my-event',
+          timestamp:
+        }
       end
     end
 
@@ -100,24 +105,25 @@ RSpec.describe EventsProcessingJob, type: :job do
     before do
       allow(Time).to receive(:now).and_return(now)
 
-      ClickHouse::Event.insert do |buffer|
+      timestamp = Time.new(2022, 7, 6, 5, 0, 0).to_i * 1000
+
+      ClickHouse::PageEvent.insert do |buffer|
         buffer << {
           uuid: SecureRandom.uuid,
           site_id: site.id,
           recording_id: recording.id,
-          type: Event::PAGE_VIEW,
-          source: nil,
-          data: { href: '/' }.to_json,
-          timestamp: Time.new(2022, 7, 6, 5, 0, 0).to_i * 1000
+          url: '/',
+          exited_at: timestamp
         }
+      end
+
+      ClickHouse::ClickEvent.insert do |buffer|
         buffer << {
           uuid: SecureRandom.uuid,
           site_id: site.id,
           recording_id: recording.id,
-          type: Event::INCREMENTAL_SNAPSHOT,
-          data: { text: 'Add to cart' }.to_json,
-          source: 2,
-          timestamp: Time.new(2022, 7, 6, 5, 0, 0).to_i * 1000
+          text: 'Add to cart',
+          timestamp:
         }
       end
     end
@@ -145,24 +151,25 @@ RSpec.describe EventsProcessingJob, type: :job do
 
       allow(EventsService::Captures).to receive(:for).and_call_original
 
-      ClickHouse::Event.insert do |buffer|
+      timestamp = Time.now.to_i * 1000
+
+      ClickHouse::PageEvent.insert do |buffer|
         buffer << {
           uuid: SecureRandom.uuid,
           site_id: site.id,
           recording_id: recording.id,
-          type: Event::META,
-          source: nil,
-          data: { href: '/' }.to_json,
-          timestamp: Time.now.to_i * 1000
+          url: '/',
+          exited_at: timestamp
         }
+      end
+
+      ClickHouse::ClickEvent.insert do |buffer|
         buffer << {
           uuid: SecureRandom.uuid,
           site_id: site.id,
           recording_id: recording.id,
-          type: Event::INCREMENTAL_SNAPSHOT,
-          data: { text: 'Add to cart' }.to_json,
-          source: 2,
-          timestamp: Time.now.to_i * 1000
+          text: 'Add to cart',
+          timestamp:
         }
       end
     end
