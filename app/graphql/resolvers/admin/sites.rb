069cebe5d6c5c024933466e0034300c9e1ca202b
Lewis Monteith <lewismonteith@gmail.com>
2022-09-27 20:17:37 +0100

    Paginate admin sites

--- app/graphql/resolvers/admin/sites.rb
+++ app/graphql/resolvers/admin/sites.rb
@@ -3,10 +3,48 @@
 module Resolvers
   module Admin
     class Sites < Resolvers::Base
-      type [Types::Sites::Site, { null: true }], null: false
+      type Types::Admin::Sites, null: false
 
-      def resolve_with_timings
-        ::Site.includes(%i[teams users]).all
+      argument :page, Integer, required: false, default_value: 1
+      argument :size, Integer, required: false, default_value: 25
+      argument :search, String, required: false, default_value: nil
+      argument :sort, Types::Admin::SiteSort, required: false, default_value: 'created_at__desc'
+
+      def resolve_with_timings(page:, size:, search:, sort:)
+        sites = ::Site
+                .includes(%i[teams users])
+                .page(page)
+                .per(size)
+                .order(order(sort))
+
+        sites = search_by(sites, search)
+
+        {
+          items: sites,
+          pagination: {
+            page_size: size,
+            total: sites.total_count,
+            sort:
+          }
+        }
+      end
+
+      private
+
+      def order(sort)
+        sorts = {
+          'created_at__asc' => 'created_at ASC',
+          'created_at__desc' => 'created_at DESC'
+        }
+        sorts[sort]
+      end
+
+      def search_by(sites, search)
+        return sites unless search
+
+        query = "%#{search}%"
+
+        sites.where('name ILIKE :query', query:)
       end
     end
   end
