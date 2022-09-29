c597908c3dd2deab7b33d076da5fd5d10a1f88b5
Lewis Monteith <lewismonteith@gmail.com>
2022-09-29 17:02:08 +0100

    Add relative position to postgres too

---
+++ db/migrate/20220929160121_add_relative_position_to_clicks.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddRelativePositionToClicks < ActiveRecord::Migration[7.0]
+  def change
+    change_table :clicks do |t|
+      t.integer :relative_to_element_x
+      t.integer :relative_to_element_y
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_29_130724) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_160121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_29_130724) do
     t.datetime "updated_at", null: false
     t.string "text"
     t.bigint "recording_id"
+    t.integer "relative_to_element_x"
+    t.integer "relative_to_element_y"
     t.index ["site_id"], name: "index_clicks_on_site_id"
   end
 
