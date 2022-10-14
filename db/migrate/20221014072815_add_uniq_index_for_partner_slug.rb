66195414895143348ff3fea99d7880a752e15b05
Lewis Monteith <lewismonteith@gmail.com>
2022-10-14 08:28:54 +0100

    Add unique index to slug

---
+++ db/migrate/20221014072815_add_uniq_index_for_partner_slug.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+
+class AddUniqIndexForPartnerSlug < ActiveRecord::Migration[7.0]
+  def change
+    add_index :partners, :slug, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_191724) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_072815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -200,6 +200,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_191724) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_partners_on_slug", unique: true
     t.index ["user_id"], name: "index_partners_on_user_id"
   end
 
