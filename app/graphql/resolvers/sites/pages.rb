30b2e57b734135347bfae75fbd66e12d4e2fbebb
Lewis Monteith <lewismonteith@gmail.com>
2022-10-30 20:53:39 +0000

    Move pages

--- app/graphql/resolvers/sites/pages.rb
+++ app/graphql/resolvers/sites/pages.rb
@@ -9,16 +9,22 @@ module Resolvers
       argument :to_date, GraphQL::Types::ISO8601Date, required: true
 
       def resolve_with_timings(from_date:, to_date:)
-        # TODO: Replace with ClickHouse
         sql = <<-SQL
-          SELECT pages.url, count(*)
-          FROM pages
-          WHERE pages.site_id = ? AND to_timestamp(pages.exited_at / 1000)::date BETWEEN ? AND ?
-          GROUP BY pages.url
-          ORDER BY count DESC
+          SELECT
+            url url,
+            count(*) count
+          FROM
+            page_events
+          WHERE
+            site_id = ? AND
+            toDate(exited_at / 1000)::date BETWEEN ? AND ?
+          GROUP BY
+            url
+          ORDER BY
+            count DESC
         SQL
 
-        Sql.execute(sql, [object.id, from_date, to_date])
+        Sql::ClickHouse.select_all(sql, [object.id, from_date, to_date])
       end
     end
   end
