25629127c3ee3bb6849a287ad80b361b0c3e44d6
Lewis Monteith <lewismonteith@gmail.com>
2022-10-31 15:39:16 +0000

    Move over countries

--- app/graphql/resolvers/analytics/per_page/countries.rb
+++ app/graphql/resolvers/analytics/per_page/countries.rb
@@ -19,17 +19,22 @@ module Resolvers
         private
 
         def countries
-          # TODO: Replace with ClickHouse
           sql = <<-SQL
-            SELECT DISTINCT(COALESCE(country_code, \'Unknown\')) country_code, COUNT(*) country_code_code
-            FROM recordings
-            INNER JOIN pages ON pages.recording_id = recordings.id
+            SELECT
+              DISTINCT(COALESCE(country_code, 'Unknown')) country_code,
+              COUNT(*) country_code_code
+            FROM
+              recordings
+            INNER JOIN
+              page_events ON page_events.recording_id = recordings.recording_id
             WHERE
               recordings.site_id = ? AND
-              to_timestamp(recordings.disconnected_at / 1000)::date BETWEEN ? AND ? AND
-              pages.url = ?
-            GROUP BY country_code
-            ORDER BY country_code_code DESC
+              toDate(recordings.disconnected_at / 1000)::date BETWEEN ? AND ? AND
+              page_events.url = ?
+            GROUP BY
+              country_code
+            ORDER BY
+              country_code_code DESC
           SQL
 
           variables = [
@@ -39,7 +44,7 @@ module Resolvers
             object.page
           ]
 
-          Sql.execute(sql, variables)
+          Sql::ClickHouse.select_all(sql, variables)
         end
       end
     end
