57a1b2aca130ee08e760fb4e3e34c483fdacd894
Lewis Monteith <lewismonteith@gmail.com>
2023-03-21 17:43:46 +0000

    Don't delete duda users if they are members of another site

--- app/services/duda_service/uninstall.rb
+++ app/services/duda_service/uninstall.rb
@@ -10,7 +10,15 @@ module DudaService
       site = ::Site.find_by!(uuid: site_name)
       return unless site
 
-      site.team.each { |team| team.user.destroy! }
+      site.team.each do |team|
+        # Only delete the duda user if they have no
+        # other sites. It could be that someone is
+        # a member of another site and we don't want
+        # to delete their user account
+        team.user.destroy! if team.user.sites.size == 1
+      end
+
+      AdminMailer.site_destroyed(site).deliver_now
       site.destroy_all_recordings!
       site.destroy!
     end
--- spec/services/duda_service/uninstall_spec.rb
+++ spec/services/duda_service/uninstall_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe DudaService::Uninstall do
     let!(:recording_1) { create(:recording, site:) }
     let!(:recording_2) { create(:recording, site:) }
 
+    let!(:team_1) { create(:team, site:, role: Team::OWNER) }
+    let!(:team_2) { create(:team, site:, role: Team::MEMBER) }
+    let!(:team_3) { create(:team, site:, role: Team::MEMBER) }
+
     subject { described_class.new(site_name: site.uuid).uninstall! }
 
     before do
-      create(:team, site:, role: Team::MEMBER)
-      create(:team, site:, role: Team::MEMBER)
-      create(:team, site:, role: Team::MEMBER)
-
       site.reload
     end
 
@@ -28,11 +28,30 @@ RSpec.describe DudaService::Uninstall do
       expect { subject }.to change { Team.all.size }.from(3).to(0)
     end
 
+    it 'deletes the users' do
+      expect { subject }.to change { User.all.size }.from(3).to(0)
+    end
+
     it 'kicks off some jobs to clean up the recordings' do
       ActiveJob::Base.queue_adapter = :test
 
       subject
       expect(RecordingDeleteJob).to have_been_enqueued.once.with(match_array([recording_1.id, recording_2.id]))
+    end
+
+    context 'if one of the members is a member of another site' do
+      before do
+        additional_site = create(:site)
+        create(:team, site: additional_site, user: team_3.user, role: Team::MEMBER)
+      end
+
+      it 'deletes the team members' do
+        expect { subject }.to change { Team.all.size }.by(-3)
+      end
+
+      it 'does not delete that users account' do
+        expect { subject }.to change { User.all.size }.from(3).to(1)
+      end
     end
   end
 end
