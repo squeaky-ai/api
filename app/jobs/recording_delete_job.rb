a9b436eec20b3572baace72abe510575f1a7b39a
Lewis Monteith <lewismonteith@gmail.com>
2023-04-12 18:58:01 +0100

    Delete visitors if they have no recordings

--- app/jobs/recording_delete_job.rb
+++ app/jobs/recording_delete_job.rb
@@ -6,19 +6,22 @@ class RecordingDeleteJob < ApplicationJob
   sidekiq_options retry: false
 
   def perform(recording_ids)
-    @recording_ids = recording_ids
     @recordings = Recording.where(id: recording_ids)
+
+    @visitor_ids = recordings.map(&:visitor_id)
+    @recording_ids = recording_ids
 
     return if @recordings.empty?
 
     delete_recordings_events
     delete_clickhouse_events
     delete_recordings
+    delete_visitors
   end
 
   private
 
-  attr_reader :recording_ids, :recordings
+  attr_reader :recording_ids, :visitor_ids, :recordings
 
   def delete_recordings_events
     # TODO: Can this be batched?
@@ -41,5 +44,21 @@ class RecordingDeleteJob < ApplicationJob
 
   def delete_recordings
     recordings.destroy_all
+  end
+
+  def delete_visitors
+    # I messed with the idea of joining recordings
+    # to visitors but I think this is the quickest
+    # way to work out if a visitor has no recordings
+    sql = <<-SQL
+      SELECT DISTINCT(visitor_id)
+      FROM recordings
+      WHERE visitor_id IN (?)
+    SQL
+
+    visitor_ids_with_recordings = Sql.execute(sql, [visitor_ids]).map { |v| v['visitor_id'] }
+    visitor_ids_to_delete = visitor_ids.filter { |v| !visitor_ids_with_recordings.include?(v) }
+
+    Visitor.where(id: visitor_ids_to_delete).destroy_all
   end
 end
--- spec/jobs/recording_delete_job_spec.rb
+++ spec/jobs/recording_delete_job_spec.rb
@@ -26,12 +26,13 @@ RSpec.describe RecordingDeleteJob, type: :job do
     described_class.perform_now([site.recordings.reload.first.id])
   end
 
-  it 'deletes the recording and all the children' do
+  it 'deletes the recording, visitor and all the clickhouse data' do
     subject
     site.reload
 
     sleep 1 # TODO: ClickHouse appears to delete async
     
+    expect(site.visitors.size).to eq(0)
     expect(site.recordings.size).to eq(0)
     expect(Sql::ClickHouse.select_value("SELECT COUNT(*) FROM click_events WHERE site_id = #{site.id}")).to eq(0)
     expect(Sql::ClickHouse.select_value("SELECT COUNT(*) FROM cursor_events WHERE site_id = #{site.id}")).to eq(0)
@@ -40,5 +41,22 @@ RSpec.describe RecordingDeleteJob, type: :job do
     expect(Sql::ClickHouse.select_value("SELECT COUNT(*) FROM page_events WHERE site_id = #{site.id}")).to eq(0)
     expect(Sql::ClickHouse.select_value("SELECT COUNT(*) FROM recordings WHERE site_id = #{site.id}")).to eq(0)
     expect(Sql::ClickHouse.select_value("SELECT COUNT(*) FROM scroll_events WHERE site_id = #{site.id}")).to eq(0)
+  end
+
+  context 'when a visitor has some other recordings that are in the data retention window' do
+    let(:visitor) { create(:visitor, site_id: site.id) }
+
+    let!(:recording_1) { create(:recording, visitor:, site:) }
+    let!(:recording_2) { create(:recording, visitor:, site:) }
+
+    subject { described_class.perform_now([recording_1]) }
+
+    it 'does not delete the visitor' do
+      subject
+      site.reload
+
+      expect(site.visitors.size).to eq(1)
+      expect(site.recordings.size).to eq(1)
+    end
   end
 end
