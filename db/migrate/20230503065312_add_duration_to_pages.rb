53cedb5834f298f290cc3ae02a237d27ef548e3a
Lewis Monteith <lewismonteith@gmail.com>
2023-05-04 16:05:58 +0100

    Add duration columns

---
+++ db/migrate/20230503065312_add_duration_to_pages.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class AddDurationToPages < ActiveRecord::Migration[7.0]
+  def up
+    add_column :pages, :duration, :bigint
+    add_column :pages, :activity_duration, :bigint
+
+    ClickHouse.connection.add_column(
+      'page_events',
+      'duration',
+      :Int64,
+      if_not_exists: true
+    )
+
+    ClickHouse.connection.add_column(
+      'page_events',
+      'activity_duration',
+      :Int64,
+      if_not_exists: true
+    )
+  end
+
+  def down
+    drop_column :pages, :duration
+    drop_column :pages, :activity_duration
+
+    ClickHouse.connection.drop_column(
+      'page_events',
+      'duration'
+    )
+
+    ClickHouse.connection.drop_column(
+      'page_events',
+      'activity_duration'
+    )
+  end
+end
