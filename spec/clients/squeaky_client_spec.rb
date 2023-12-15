5636aa60fbafe0ec72b31ae130cab10818af19af
Lewis Monteith <lewismonteith@gmail.com>
2023-12-15 17:02:21 +0000

    Add some missing speccs

---
+++ spec/clients/squeaky_client_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SqueakyClient do
+  before do
+    ENV['SQUEAKY_API_KEY'] = 'test'
+    allow(described_class).to receive(:post)
+  end
+
+  describe '#add_event' do
+    let(:name) { 'event_name' }
+    let(:data) { { foo: 'bar' } }
+    let(:user_id) { SecureRandom.uuid }
+
+    subject { described_class.new.add_event(name:, data:, user_id:) }
+
+    it 'sends the expected payload' do
+      subject
+      expect(described_class).to have_received(:post).with(
+        '/api/events',
+        body: {
+          name:,
+          user_id:,
+          data: data.to_json,
+        }.to_json,
+        headers: {
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json',
+          'X-SQUEAKY-API-KEY' => 'test'
+        },
+        timeout: 5
+      )
+    end
+  end
+end
---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Auth::SessionsController, type: :request do
+  describe 'POST /api/auth/sign_in' do
+    let(:headers) do
+      {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      }
+    end
+
+    subject { post '/api/auth/sign_in', params: params.to_json, headers: }
+
+    context 'when the user does not exist' do
+      let(:params) do
+        {
+          user: {
+            email: 'sdklfsdj@gmail.com',
+            password: 'sdfsdfsfsd'
+          }
+        }
+      end
+
+      it 'returns an unsuccessful response' do
+        subject
+        expect(response.status).to eq(401)
+        expect(response.body).to include('Invalid Email or password')
+      end
+    end
+
+    context 'when the user exists but the password is wrong' do
+      let(:user) { create(:user) }
+
+      let(:params) do
+        {
+          user: {
+            email: user.email,
+            password: 'sdfsdfsfsd'
+          }
+        }
+      end
+
+      it 'returns an unsuccessful response' do
+        subject
+        expect(response.status).to eq(401)
+        expect(response.body).to include('Invalid Email or password')
+      end
+    end
+
+    context 'when the user exists and the password is correct' do
+      let(:user) { create(:user) }
+
+      let(:params) do
+        {
+          user: {
+            email: user.email,
+            password: user.password
+          }
+        }
+      end
+
+      it 'returns an successful response' do
+        subject
+        expect(response.status).to eq(200)
+        expect(response.parsed_body['id']).to eq(user.id)
+        expect(response.parsed_body['first_name']).to eq(user.first_name)
+        expect(response.parsed_body['last_name']).to eq(user.last_name)
+        expect(response.parsed_body['email']).to eq(user.email)
+      end
+    end
+  end
+
+  describe 'DELETE /api/auth/sign_out' do
+    let(:headers) do
+      {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      }
+    end
+
+    subject { delete '/api/auth/sign_out', headers: }
+
+    it 'returns a 204 status' do
+      subject
+      expect(response.status).to eq(204)
+    end
+  end
+end
