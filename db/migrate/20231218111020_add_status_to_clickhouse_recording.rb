3437fe25ac0355ea17522699ba97c58cb46969e2
Lewis Monteith <lewismonteith@gmail.com>
2023-12-18 12:29:00 +0000

    Add status column to clickhouse

---
+++ db/migrate/20231218111020_add_status_to_clickhouse_recording.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class AddStatusToClickhouseRecording < ActiveRecord::Migration[7.1]
+  def up
+    ClickHouse.connection.add_column(
+      'recordings',
+      'status',
+      :Int16,
+      if_not_exists: true
+    )
+  end
+
+  def down
+    ClickHouse.connection.drop_column(
+      'recordings',
+      'status'
+    )
+  end
+end
