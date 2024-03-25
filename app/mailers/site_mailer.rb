b099b85cd33faa3c2f0bcca6e6bf63380b2b7bad
Lewis Monteith <lewismonteith@gmail.com>
2024-03-25 13:19:42 +0000

    Remove weekly review email

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -9,16 +9,6 @@ class SiteMailer < ApplicationMailer
     mail(to: team.user.comms_email, subject:)
   end
 
-  def weekly_review(site, data, user)
-    @site = site
-    @data = data
-    @unsubscribable = true
-
-    return unless user.communication_enabled?(:weekly_review_email)
-
-    mail(to: user.comms_email, subject: 'Your Week In Review')
-  end
-
   def plan_exceeded(site, data, user)
     @site = site
     @user = user
--- app/services/weekly_review_service/base.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module WeeklyReviewService
-  class Base
-    def self.milliseconds_to_mmss(milliseconds = 0)
-      Time.at(milliseconds.abs / 1000).utc.strftime('%-Mm %-Ss')
-    end
-  end
-end
